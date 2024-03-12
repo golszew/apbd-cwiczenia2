33c22dc5362c71128be55837040e808d06a18cae
s27691 <s27691@pjwstk.edu.pl>
2024-03-12 11:29:35 +0100

    Add GetAverage from numbers

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -1,6 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Globalization;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Modyfikcja1");
 Console.WriteLine("Modyfikcja2");
 Console.WriteLine("Modyfikcja3");
+
+static double GetAverage(int[] numbers)
+{
+    int sum = 0;
+    foreach (var number in numbers)
+    {
+        sum += number;
+    }
+
+    return (dobule) sum / numbers.Length;
+    
+}
